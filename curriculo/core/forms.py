9874c2d76070ceb0bbe41d8f2f4ca0d78c105b5d
Vinicius Fernandes <viniciusfdossantos@yahoo.com>
2022-03-31 20:20:17 -0300

    finalizando projeto

--- curriculo/core/forms.py
+++ curriculo/core/forms.py
@@ -10,5 +10,17 @@ class NewCandidatoForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['idiomas'].widget.attrs.update({'placeholder':'Teste'})
-        self.fields['email'].widget.attrs.update({'placeholder':'email@provedor.com'})
+        self.fields['endereco'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: Centro, Volta Redonda - RJ'})
+        self.fields['email'].widget.attrs.update({'class':'special-placeholders', 'placeholder':'Ex: email@provedor.com'})
+        self.fields['telefone'].widget.attrs.update({'class':'special-placeholders', 'placeholder':'Ex: (24) 9983-6570'})
+        self.fields['linkedin'].widget.attrs.update({'class':'special-placeholders', 'placeholder':'Ex:  https://linkedin.com/ronaldocerqueira'})
+        self.fields['formacao_academica'].widget.attrs.update({'class':'special-placeholders', 'placeholder':'Ex: ', 'rows':'4'})
+        self.fields['idiomas'].widget.attrs.update({'class':'special-placeholders','placeholder':'Inglês Básico - Espanhol Fluente'})
+        self.fields['outras_qualificacoes'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex:', 'rows':'4'})
+        self.fields['empresa1'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: Contabilidade União - 03/2018 a 02/2022'})
+        self.fields['experiencia1'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: ', 'rows':'4'})
+        self.fields['empresa2'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: Laboratórios Lux - 01/2011 a 08/2016'})
+        self.fields['experiencia2'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: ', 'rows':'4'})
+        self.fields['empresa3'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: Mecânica Teixeira - 05/2009 a 06/2010'})
+        self.fields['experiencia3'].widget.attrs.update({'class':'special-placeholders','placeholder':'Ex: ', 'rows':'4'})
+
